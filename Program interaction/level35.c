d2c25d23d3f57b88a7e767735f62823ce81f8661
Abdouomar22 <117710341+Abdouomar22@users.noreply.github.com>
2024-04-05 06:15:37 +0100

    Create level35.c

---
+++ Program interaction/level35.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int pwncollege() {
+    pid_t pid = fork(); // Create a child process
+
+    if (pid == -1) {
+        // Fork failed
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Child process
+        char *binaryPath = "/bin/env";
+
+        char *args[] = {"env","-i","LC_CTYPE=En_us","/challenge/embryoio_level35",NULL};
+        
+        // Execute the binary program in the child process
+        execv(binaryPath, args);
+        
+        // If execv returns, an error occurred
+        perror("execv");
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process
+        // Wait for the child process to finish
+        wait(NULL);
+    }
+
+    return 0;
+}
+
+int main(){
+ 
+   pwncollege();
+ return 0;
+}
